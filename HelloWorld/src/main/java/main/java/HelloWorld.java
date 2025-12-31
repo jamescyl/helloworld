79dd53419beaac31663c40b88a24eb5a672a40de
jc <cyljames@gmail.com>
2025-12-31 16:31:44 +0800

    4 x helloworld

--- HelloWorld/src/main/java/main/java/HelloWorld.java
+++ HelloWorld/src/main/java/main/java/HelloWorld.java
@@ -6,6 +6,5 @@ public class HelloWorld {
 		// TODO Auto-generated method stub
 		System.out.println("Hello world!");
 		System.out.println("Hello world!");
-		System.out.println("Hello world!");
 	}
 }
