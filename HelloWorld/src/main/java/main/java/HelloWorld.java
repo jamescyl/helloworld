283d874dca0acdd65f61b688f87cb5a07cb67e56
jc <cyljames@gmail.com>
2025-12-31 10:12:28 +0800

    Commit src 3x hello world

--- HelloWorld/src/main/java/main/java/HelloWorld.java
+++ HelloWorld/src/main/java/main/java/HelloWorld.java
@@ -6,5 +6,6 @@ public class HelloWorld {
 		// TODO Auto-generated method stub
 		System.out.println("Hello world!");
 		System.out.println("Hello world!");
+		System.out.println("Hello world!");
 	}
 }
