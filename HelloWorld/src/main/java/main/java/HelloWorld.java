361349a10240cfac0efc44e3d6bf26f68a0f4bea
jc <cyljames@gmail.com>
2025-12-31 10:10:29 +0800

    2nd commit src -> double HelloWorld

--- HelloWorld/src/main/java/main/java/HelloWorld.java
+++ HelloWorld/src/main/java/main/java/HelloWorld.java
@@ -5,6 +5,6 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello world!");
-		System.out.println("Good day");
+		System.out.println("Hello world!");
 	}
 }
